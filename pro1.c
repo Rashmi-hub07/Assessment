0eae125b527b23c0ca1c03ad3c60d2ac3c20e081
rashmi <rashmi.c@s.amity.edu>
2025-05-06 21:45:39 +0530

    c1

--- pro1.c
+++ pro1.c
@@ -5,7 +5,7 @@ int main() {
    num1=4;
    num2=2;
         result = num1*num2;
-        printf("Result: %d", result);
+        printf("The multiple of num1 and num2 is %d", result);
 
     return 0;
 }
