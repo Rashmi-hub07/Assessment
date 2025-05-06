ce2fd26ea0b3f873ee3ef4bf252f9c972e4d3c55
rashmi <rashmi.c@s.amity.edu>
2025-05-06 22:06:50 +0530

    prime number into 5 divided by 2

---
+++ fun.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	int i, j, prime;
+
+	for( i=5; i<=50; i++ ) {
+		prime = 1;
+		 for( j=2; j<i; j++ ) {
+			 if( i % j == 0 ) {
+				 prime = 0:
+		break;
+	}
+    }
+		 if ( prime && ((i* 5) % 2 == 0)) {
+			 printf("%d",i);
+		 }
+	}
+
+	return 0;
+}
