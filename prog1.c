532e6bed7287d0752fc1f6dc4db26d22f9d1b67d
rashmi <rashmi.c@s.amity.edu>
2025-05-06 12:09:36 +0530

    sub1

--- prog1.c
+++ prog1.c
@@ -1,14 +1,11 @@
-Here's a simple C program to perform division:
 #include <stdio.h>
 
 int main() {
     float num1, num2, result;
 
-    // Taking input
     printf("Enter two numbers: ");
     scanf("%f %f", &num1, &num2);
 
-    // Checking for division by zero
     if (num2 != 0) {
         result = num1 / num2;
         printf("Result: %.2f\n", result);
