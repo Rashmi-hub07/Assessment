33609da6ab42da40c7e6091f031c012a434937d6
rashmi <rashmi.c@s.amity.edu>
2025-05-06 12:28:21 +0530

    c2

---
+++ pro1.c
@@ -0,0 +1,12 @@
+Here's a simple C program to perform division:
+#include <stdio.h>
+
+int main() {
+    int num1, num2, result;
+   num1=4;
+   num2=2;
+        result = num1 / num2;
+        printf("Result: %d", result);
+
+    return 0;
+}
